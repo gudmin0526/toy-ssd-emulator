beff8f2bfb2e9023e8b4246a1995448308c98340
gudmin0526 <gudmin0526@gmail.com>
2026-01-07 14:59:58 +0900

    Add main ctrl

---
+++ ssd/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include "ssd/ssd.h"
+
+int main(int argc, char* argv[]) {
+    char op = argv[0][0];
+    char _addr[128] = { 0, };
+    char _value[128] = { 0, };
+
+    strcpy(_addr, argv[1]); 
+    uint8_t addr = (uint8_t)atoi(_addr);
+
+    int32_t value = 0;
+
+    switch (op) {
+    case 'R':
+        read_block(addr, &value);
+        return value;
+    case 'W':
+        value = (int32_t)atoi(_value);
+        write_block(addr, value);
+    case 'D':
+        delete_block(addr);
+    default:
+        break;
+    }
+
+    return 0;
+}
