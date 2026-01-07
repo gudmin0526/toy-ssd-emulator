000c0e00dd1503498707c176086d878fcdf3e187
jdong800@naver.com <jdong800@naver.com>
2026-01-07 15:21:37 +0900

    write 함수 수정, delete 기능 구현

--- ssd/ssd.c
+++ ssd/ssd.c
@@ -3,18 +3,32 @@
 #include <string.h>
 #include "ssd.h"
 
-static uint32_t used[NUM_META_BLOCK] = { 0,  };
-static block_t SSD[NUM_BLOCK] = { 0,  };
+// static uint32_t used[NUM_META_BLOCK] = { 0,  };
+// static block_t SSD[NUM_BLOCK] = { 0,  };
+static ssd_t SSD;
 
 /* write, delete 업데이트 발생 시 1 증가 */
-static uint8_t dirty_count = 0;
 static uint32_t log_seq = 0;
 
-uint8_t write_block(uint8_t addr, int32_t data) {    
-    SSD[addr].data = data;
-    flush_ssd();
+ret_t write_block(uint8_t addr, int32_t data) {
+    // SSD[addr].data = data;
+    if ((addr < NUM_META_BLOCK) || (addr >= NUM_BLOCK)) {
+        printf("write_block: write out of index!\n");
+        return WRITE_OUT_OF_INDEX;
+    }
 
-    printf("write_block: addr: %d, data: %d\n", addr, SSD[addr].data);
+    SSD.used[addr] = true;
+    SSD.block[addr].data = data;
+    ret_t result = flush_ssd();
+
+    if (result != SSD_OK) {
+        printf("write_block: write failed!\n");
+        return WRITE_FAIL;
+    }
+
+    // printf("write_block: addr: %d, data: %d\n", addr, SSD[addr].data);
+    printf("write_block: addr: %d, data: %d\n", addr, SSD.block[addr].data);
+    return result;
 }
 
 static void log_msg(const char* msg) {
@@ -27,9 +41,7 @@ static void log_msg(const char* msg) {
 
 uint8_t init_ssd(void) {
     /* 전역 변수 초기화 */
-    memset(SSD, 0, sizeof(block_t) * NUM_BLOCK);
-    memset(used, 0, sizeof(uint32_t) * NUM_META_BLOCK);
-    dirty_count = 0;
+    memset(SSD.block, 0, sizeof(block_t) * NUM_BLOCK);
 
     /* 파일 오픈 */
     FILE *ssd_fp = fopen("ssd.txt", "rb+");
@@ -44,12 +56,12 @@ uint8_t init_ssd(void) {
     }
 
     /* 파일을 SSD 배열로 읽어온다 */
-    size_t n_read = fread(SSD, sizeof(block_t), NUM_BLOCK, ssd_fp);
+    size_t n_read = fread(SSD.block, sizeof(block_t), NUM_BLOCK, ssd_fp);
 
-    /* used 배열로 읽어온다. */
-    for (size_t i = 0; i < NUM_META_BLOCK; i++) {
-        used[i] = (uint32_t)SSD[i].data;
-    }
+    // /* used 배열로 읽어온다. */
+    // for (size_t i = 0; i < NUM_META_BLOCK; i++) {
+    //     used[i] = (uint32_t)SSD.block[i].data;
+    // }
     fclose(ssd_fp);
 
     sleep_ms(1000);
@@ -60,28 +72,44 @@ uint8_t init_ssd(void) {
 void dump(void) {
     printf("---dump---\n");
     for (int i = 0; i < MAX_SIZE; ++i) {
-        printf("%d: %d\n", i, SSD[i].data);
+        printf("%d: %d\n", i, SSD.block[i].data);
     }
 }
 
-static uint8_t flush_ssd(void) {
+static ret_t flush_ssd(void) {
     /* 기존에 존재하는 파일 오픈 */
     FILE *ssd_fp = fopen("ssd.txt", "rb+");
 
     if (ssd_fp == NULL) {
         /* 파일 없으면 실패 반환 */
-        return 1;
+        return WRITE_FAIL;
     }
-    fwrite(SSD, sizeof(block_t), NUM_BLOCK, ssd_fp);
+    fwrite(SSD.block, sizeof(block_t), NUM_BLOCK, ssd_fp);
     fclose(ssd_fp);
 
-    return 0;
+    return SSD_OK;
 }
 
 static uint8_t integrity_check(void) {
 
 }
 
-uint8_t delete_block(uint8_t addr) {
-    
+ret_t delete_block(uint8_t addr) {
+    ret_t result = SSD.used[addr] ? SSD_OK : DELETE_FAIL_NOT_IN_USED;
+
+    if (result != SSD_OK) {
+        printf("delete_block: block not in used!\n");
+        return DELETE_FAIL_NOT_IN_USED;
+    }
+
+    result = write_block(addr, 0);
+
+    if (result != SSD_OK) {
+        printf("delete_block: delete failed!\n");
+        return DELETE_FAIL_CLEAR_FAILED;
+    }
+
+    SSD.used[addr] = false;
+
+    return result;
 }
--- ssd/ssd.h
+++ ssd/ssd.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
 
 /* UNIX, WINDOWS 크로스 플랫폼 이식성 유지 */
 #ifdef _WIN32
@@ -22,21 +23,33 @@
 #define NUM_META_BLOCK 4
 #define NUM_DATA_BLOCK 96
 
+typedef enum ret {
+	SSD_OK = 0,
+	WRITE_FAIL = 1,
+	WRITE_OUT_OF_INDEX = 2,
+	DELETE_FAIL_NOT_IN_USED = 3,
+	DELETE_FAIL_CLEAR_FAILED = 4,
+} ret_t;
+
 typedef struct block {
 	int32_t data;
 } block_t;
+
+typedef union ssd {
+	bool used[NUM_DATA_BLOCK];
+	block_t block[NUM_BLOCK];
+} ssd_t;
 
 /* SSD 초기화 */
 uint8_t init_ssd(void);
 
 /* CRUD */
-uint8_t write_block (uint8_t addr, int32_t data);
+ret_t write_block(uint8_t addr, int32_t data);
 uint8_t read_block  (uint8_t addr, int32_t* out_data);
-uint8_t delete_block(uint8_t addr);
+ret_t delete_block(uint8_t addr);
 
 void dump(void);
-static uint8_t flush_ssd(void);
+static ret_t flush_ssd(void);
 static uint8_t integrity_check(void);
-uint8_t delete_block(uint8_t addr);
 
 #endif
