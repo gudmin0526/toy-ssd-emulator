e69e0ab95d5beebbeafc8e4f073b62f408c48904
gudmin0526 <gudmin0526@gmail.com>
2026-01-07 11:33:11 +0900

    Add API

--- ssd/ssd.c
+++ ssd/ssd.c
@@ -1,4 +1,4 @@
 #include "ssd.h"
 
 uint8_t used[5][4];
-block_t SSD[MAX_SIZE];
+block_t SSD[SSD_SIZE];
--- ssd/ssd.h
+++ ssd/ssd.h
@@ -3,10 +3,14 @@
 
 #include <stdint.h>
 
-#define MAX_SIZE 100
+#define SSD_SIZE 100
 
 typedef struct block {
 	int32_t data;
 } block_t;
+
+uint8_t write_block (uint8_t addr, int32_t data);
+uint8_t read_block  (uint8_t addr, int32_t* out_data);
+uint8_t delete_block(uint8_t addr);
 
 #endif
